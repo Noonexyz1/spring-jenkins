9ffb8f6c690331adff5f426137b9f0eaafe09b94
Noonexyz1 <aries-d2github@hotmail.com>
2024-07-22 17:07:23 -0400

    refactor: test 3

--- src/main/java/com/prueba/Controller.java
+++ src/main/java/com/prueba/Controller.java
@@ -8,7 +8,7 @@ public class Controller {
 
     @GetMapping("/saludo")
     public String saludo() {
-        return "Hello Polled Builds, test 2";
+        return "Hello Polled Builds, test 3";
     }
 }
 
