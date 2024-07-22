adf31568bb008cb7d4e27d12b34f6f5e65adde82
Noonexyz1 <aries-d2github@hotmail.com>
2024-07-22 14:44:17 -0400

    Primer commit

---
+++ src/main/java/com/prueba/Controller.java
@@ -0,0 +1,15 @@
+package com.prueba;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/saludo")
+    public String saludo() {
+        return "Hola Jenkins";
+    }
+}
+
+
